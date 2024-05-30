56b7e6bd6e8ed987d76fa709ede30e76233465f9
MaxymChyncha <max.chyncha@gmail.com>
2024-05-30 10:22:23 +0300

    feat(user): implement register feature for user

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from user.views import UserRegisterView
+
+urlpatterns = [
+    path("register/", UserRegisterView.as_view(), name="register"),
+]
 
 app_name = "user"
