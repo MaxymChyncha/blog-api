3986fad58c259413e5d0727d8c306772b4c736e9
MaxymChyncha <max.chyncha@gmail.com>
2024-05-30 10:39:44 +0300

    feat(user): implement login & logout feature for user

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
@@ -6,7 +6,11 @@ User = get_user_model()
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
     password_2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
@@ -29,3 +33,17 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password_2")
         return User.objects.create_user(**validated_data)
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        if user := authenticate(email=email, password=password):
+            data["user"] = user
+            return data
+        raise serializers.ValidationError("Your Email or password is incorrect.")
--- user/views.py
+++ user/views.py
@@ -1,9 +1,39 @@
-from rest_framework import generics
+from django.contrib.auth import logout
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from user.serializers import UserRegisterSerializer
+from user.serializers import UserRegisterSerializer, LoginSerializer
 
 
 class UserRegisterView(generics.CreateAPIView):
     serializer_class = UserRegisterSerializer
     permission_classes = (AllowAny,)
+
+
+class LoginView(ObtainAuthToken):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(
+            {"message": "You successfully logged out"},
+            status=status.HTTP_200_OK
+        )
