f70612d59878bb118f1bc71e0d9c183ed4c5da7b
MaxymChyncha <max.chyncha@gmail.com>
2024-05-30 10:39:44 +0300

    feat(user): implement login & logout feature for user

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF settings
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+}
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user.views import UserRegisterView
+from user.views import UserRegisterView, LoginView, LogoutView
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
 
 app_name = "user"
