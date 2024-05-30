37c62e3be7cfc5480dc44c6d8c7f4e395fad3465
MaxymChyncha <max.chyncha@gmail.com>
2024-05-30 10:45:36 +0300

    feat(user): implement change password feature for user

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from user.views import UserRegisterView, LoginView, LogoutView
+from user.views import (
+    UserRegisterView,
+    LoginView,
+    LogoutView,
+    UserChangePasswordView,
+)
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("change_password/", UserChangePasswordView.as_view(), name="change_password"),
 ]
 
 app_name = "user"
