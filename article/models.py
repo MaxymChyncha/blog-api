be0809c61bcc18093bb79a6577456087eb5f1fca
MaxymChyncha <max.chyncha@gmail.com>
2024-05-30 18:31:07 +0300

    feat(article): implement article model & register to admin panel

--- article/models.py
+++ article/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publication_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.title
