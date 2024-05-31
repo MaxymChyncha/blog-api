8117cc5ee837c3348f5b431a765bbd49fea52158
MaxymChyncha <max.chyncha@gmail.com>
2024-05-31 08:44:36 +0300

    feat(article): implement CRUD for article model

--- article/urls.py
+++ article/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from article.views import ArticleViewSet
+
+router = DefaultRouter()
+router.register("articles", ArticleViewSet, basename="article")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
 
 app_name = "article"
