b861fd6294899e3f8ec0b681a6321213c43a94b0
MaxymChyncha <max.chyncha@gmail.com>
2024-06-04 16:10:13 +0300

    feat(article): add signal for sending notification in telegram bot

--- article/models.py
+++ article/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 class Article(models.Model):
     """
-    Serializer for the Article model.
+    Model representing an article in the blog.
 
-    This serializer handles the serialization and deserialization of
-    Article instances.
+    Attributes:
+        title (str): The title of the article. Maximum length is 255 characters.
+        content (str): The main content of the article.
+        publication_date (datetime): The date and time when the article was published.
+                                     Automatically set to the current date and time when the article is created.
+        author (User): The author of the article. References a user from the user model.
     """
     title = models.CharField(max_length=255)
     content = models.TextField()
