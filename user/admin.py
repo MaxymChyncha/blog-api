ea1568eb573e4e3867d341e95efbfff560cb0f42
MaxymChyncha <max.chyncha@gmail.com>
2024-06-09 19:32:58 +0300

    feat(user): implement model for PasswordResetToken & register to admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from user.models import User
+from user.models import User, PasswordResetToken
 from django.contrib.auth.models import Group
 from django.utils.translation import gettext as _
 
@@ -23,6 +23,14 @@ class UserAdmin(DjangoUserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
+
+
+@admin.register(PasswordResetToken)
+class PasswordResetTokenAdmin(admin.ModelAdmin):
+    list_display = ("user", "token", "created_at", "expires_at")
+    search_fields = ("user__username", "user__email", "token")
+    list_filter = ("created_at", "expires_at")
+    ordering = ("-created_at",)
 
 
 admin.site.unregister(Group)
